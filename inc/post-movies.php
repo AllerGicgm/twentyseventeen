956b86874aab5d02990c16647b7356ce857f4df1
AllerGicgm <32534492+AllerGicgm@users.noreply.github.com>
2017-10-07 05:46:45 +0300

    Update post-movies.php

--- inc/post-movies.php
+++ inc/post-movies.php
@@ -61,7 +61,14 @@ function twentyseventeen_mb_callback($post){
 	if (!empty($mb_data)) {
 		$sub = $mb_data[0];
 	}
-	echo '<label>Subtitle</label><br/><input name="2017_subtitle" type=text value="'.$sub.'">';
+	echo '<label>Subtitle</label><br/><input name="2017_subtitle" type=text value="'.$sub.'"><br/>';
+
+	$mb_data = get_post_meta( $post->ID, '_price' );
+	$price = '';
+	if (!empty($mb_data)) {
+		$price = $mb_data[0];
+	}
+	echo '<label>Price</label><br/><input name="price" type=text value="'.$price.'">';
 }
 
 /*
@@ -75,9 +82,13 @@ function twentyseventeen_metabox_save($pid, $post) {
 		if ( !current_user_can( 'edit_post', $post->ID ) ) return;
 
 		$data = $_POST['2017_subtitle'];
-
 		if (!empty($data)) {
 			update_post_meta($pid, '2017_mb', $data);
+		}
+
+		$price = $_POST['price'];
+		if (!empty($data)) {
+			update_post_meta($pid, '_price', $price);
 		}
 	}
 }
@@ -104,6 +115,81 @@ function wc_qb_redirect($url) {
 	}
 	return $url;
 }
+
+if (class_exists('WC_Product_Data_Store_CPT')) {
+	class 2017_Product_Data_Store_CPT extends WC_Product_Data_Store_CPT {
+
+		/**
+		 * Method to read a product from the database.
+		 * @param WC_Product
+		 */
+
+		public function read( &$product ) {
+
+			$product->set_defaults();
+
+			if ( ! $product->get_id() || ! ( $post_object = get_post( $product->get_id() ) ) || ! in_array( $post_object->post_type, array( 'movie', 'product' ) ) ) { // change birds with your post type
+				throw new Exception( __( 'Invalid product.', 'woocommerce' ) );
+			}
+
+			$id = $product->get_id();
+
+			$product->set_props( array(
+				'name'              => $post_object->post_title,
+				'slug'              => $post_object->post_name,
+				'date_created'      => 0 < $post_object->post_date_gmt ? wc_string_to_timestamp( $post_object->post_date_gmt ) : null,
+				'date_modified'     => 0 < $post_object->post_modified_gmt ? wc_string_to_timestamp( $post_object->post_modified_gmt ) : null,
+				'status'            => $post_object->post_status,
+				'description'       => $post_object->post_content,
+				'short_description' => $post_object->post_excerpt,
+				'parent_id'         => $post_object->post_parent,
+				'menu_order'        => $post_object->menu_order,
+				'reviews_allowed'   => 'open' === $post_object->comment_status,
+			) );
+
+			$this->read_attributes( $product );
+			$this->read_downloads( $product );
+			$this->read_visibility( $product );
+			$this->read_product_data( $product );
+			$this->read_extra_data( $product );
+			$product->set_object_read( true );
+		}
+
+		/**
+		 * Get the product type based on product ID.
+		 *
+		 * @since 3.0.0
+		 * @param int $product_id
+		 * @return bool|string
+		 */
+		public function get_product_type( $product_id ) {
+			$post_type = get_post_type( $product_id );
+			if ( 'product_variation' === $post_type ) {
+				return 'variation';
+			} elseif ( in_array( $post_type, array( 'movie', 'product' ) ) ) { // change birds with your post type
+				$terms = get_the_terms( $product_id, 'product_type' );
+				return ! empty( $terms ) ? sanitize_title( current( $terms )->name ) : 'simple';
+			} else {
+				return false;
+			}
+		}
+	}
+}
+
+add_filter( 'woocommerce_data_stores', 'woocommerce_data_stores' );
+
+function woocommerce_data_stores ( $stores ) {
+	$stores['product'] = '2017_Product_Data_Store_CPT';
+	return $stores;
+}
+
+function wc_product_class( $class, $product_type, $post_type ) {
+	if( 'movie' == $post_type )
+		$class = 'WC_Product_Simple';
+	return $class;
+}
+
+add_filter( 'woocommerce_product_class', 'wc_product_class', 10, 3);
 
 /*
 Adding custom registration field
